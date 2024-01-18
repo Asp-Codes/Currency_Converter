463d1c4fe07c7c4fa7cad676fd34702c1413214d
Hemanth-Asp <punetihemanth@iitbhilai.ac.in>
2024-01-18 18:24:07 +0530

    update_symbol_functionality

--- index.js
+++ index.js
@@ -2,19 +2,13 @@ const baseUrl ="https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/cu
 
 const dropdown=document.querySelectorAll(".dropdown select");
 const Btn=document.querySelector("form button");
-const fromCurr=document.querySelector(".from select");
-const toCurr=document.querySelector(".to select");
+var fromCurr=document.querySelector(".from select");
+var toCurr=document.querySelector(".to select");
 const message=document.querySelector("form .msg");
 const swap=document.querySelector("form #swap");
 
-swap.addEventListener("click",()=>{
-    const temp=fromCurr;
-    fromCurr=toCurr;
-    toCurr=temp;
-});
 
-
-for(let select of dropdown)
+for(let select of dropdown){
 for (currCode in countryList){
     let newOption=document.createElement("option");
     newOption.innerText=currCode;
@@ -25,14 +19,48 @@ for (currCode in countryList){
     if(select.name ==="to" && currCode === "INR"){
         newOption.selected="selected";
     }
-
     select.append(newOption);
     select.addEventListener("change",(evt)=>{
         // console.log(evt);
         updateFlag(evt.target);
         // console.log(evt.target);
     })
-}
+}}
+
+swap.addEventListener("click",()=>{
+    var temp=fromCurr;
+    fromCurr=toCurr;
+    toCurr=temp;
+    console.log(fromCurr," ",fromCurr.value);
+    for (let i = 0; i < fromCurr.options.length; i++) {
+            if(fromCurr.options[i].selected === true && fromCurr.options[i].innerText !== fromCurr.value){
+                fromCurr.options[i].selected=false;
+            }
+    
+            if (fromCurr.options[i].innerText === fromCurr.value ) {
+               
+                fromCurr.options[i].selected = true;
+                //  console.log(toCurr.options[i].selected);
+                //  console.log("is the value");
+                 updateSymbolFrom(fromCurr);
+                break; 
+            }
+    }
+    for (let i = 0; i < toCurr.options.length; i++) {
+        if(toCurr.options[i].selected === true && toCurr.options[i].innerText !== toCurr.value){
+            toCurr.options[i].selected=false;
+        }
+
+        if (toCurr.options[i].innerText === toCurr.value ) {
+           
+            toCurr.options[i].selected = true;
+             console.log(toCurr.options[i].selected);
+             console.log("is the value");
+             updateSymbolTo(toCurr);;
+            break; 
+        }
+    }
+});
 
 
 const updateFlag =(element) =>{
@@ -40,11 +68,30 @@ const updateFlag =(element) =>{
     let countryCode=countryList[currCode];
     let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
     let img=element.parentElement.querySelector("img");
+    // console.log(element.parentElement);
+    img.src=newSrc;
+}
+
+const updateSymbolTo = (element)=>{
+    let currCode=element.value;
+    let countryCode=countryList[currCode];
+    let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+    let img=document.getElementById("to_flag");
+    // console.log(element.parentElement);
+    img.src=newSrc;
+}
+
+const updateSymbolFrom = (element)=>{
+    let currCode=element.value;
+    let countryCode=countryList[currCode];
+    let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+    let img=document.getElementById("from_flag");
+    // console.log(element.parentElement);
     img.src=newSrc;
 }
 
 
-    Btn.addEventListener("click",async (evt)=>{
+Btn.addEventListener("click",async (evt)=>{
     evt.preventDefault();
     let amount=document.querySelector(".amount input");
     let amtVal=amount.value;
